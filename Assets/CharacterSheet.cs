d58dd0316e32d89c7b09238bcdbf8c2c4af0c506
Omenys <105769289+Omenys@users.noreply.github.com>
2024-02-03 04:29:52 -0600

    Private variables added

--- Assets/CharacterSheet.cs
+++ Assets/CharacterSheet.cs
@@ -6,12 +6,17 @@ using UnityEngine;
 public class CharacterSheet : MonoBehaviour
 {
 
-    // Variables visible in Unity
+    // Variables public in Unity
     [SerializeField] string characterName = "Tav";
     [SerializeField] int proficiency = 0;
     [SerializeField] bool usingFinesseWeapon;
     [SerializeField] [Range (-5, 5)] int strengthModifier = 0;
     [SerializeField] [Range (-5, 5)] int dexterityModifier = 0;
+
+    // Variables private
+    private int hitModifier = 0;
+    private int armorClass = 0;
+    private bool isHit = false;
 
     // Start is called before the first frame update
     void Start()
