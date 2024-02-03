d3a1b30ab512e7541892774be7a2a8d7802340e4
Omenys <105769289+Omenys@users.noreply.github.com>
2024-02-03 04:08:36 -0600

    Variables created

--- Assets/CharacterSheet.cs
+++ Assets/CharacterSheet.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class CharacterSheet : MonoBehaviour
 {
+
+    // Variables visible in Unity
+    [SerializeField] string characterName = "Tav";
+    [SerializeField] int profBonus = 0;
+    [SerializeField] bool usingFinesseWeapon;
+    [SerializeField] [Range (-5, 5)] int strengthModifier = 0;
+    [SerializeField] [Range (-5, 5)] int dexterityModifier = 0;
+
     // Start is called before the first frame update
     void Start()
     {
